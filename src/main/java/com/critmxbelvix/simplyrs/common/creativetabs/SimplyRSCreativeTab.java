f8fbb83f351de5cf4b7dd657d2fe65aea11c5d60
CriTMx <savefailedmc@gmail.com>
2022-12-09 13:47:42 +0530

    fixed unresolved reference

--- src/main/java/com/critmxbelvix/simplyrs/common/creativetabs/SimplyRSCreativeTab.java
+++ src/main/java/com/critmxbelvix/simplyrs/common/creativetabs/SimplyRSCreativeTab.java
@@ -5,13 +5,14 @@ import com.critmxbelvix.simplyrs.common.registers.ItemRegister;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public class SimplyRSCreativeTab
 {
     public static final CreativeModeTab SRS_TAB = new CreativeModeTab("srscreativetab") {
-        @Override
+        @Override @NotNull
         public ItemStack makeIcon() {
-            return new ItemStack(BlockRegister.LOGICGATEOR.get());
+            return new ItemStack(BlockRegister.LOGICGATE_OR.get());
         }
     };
 }
