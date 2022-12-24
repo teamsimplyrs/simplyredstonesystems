7d242e4d155d7a95ffaadb9450f8abad98d548c1
CriTMx <savefailedmc@gmail.com>
2022-12-24 17:20:25 +0530

    added a new creativetab for purely crafting resources

--- src/main/java/com/critmxbelvix/simplyrs/common/creativetabs/SimplyRSCreativeTab.java
+++ src/main/java/com/critmxbelvix/simplyrs/common/creativetabs/SimplyRSCreativeTab.java
@@ -1,8 +1,10 @@
 package com.critmxbelvix.simplyrs.common.creativetabs;
 
 import com.critmxbelvix.simplyrs.common.registers.BlockRegister;
+import com.critmxbelvix.simplyrs.common.registers.ItemRegister;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import org.checkerframework.checker.units.qual.C;
 import org.jetbrains.annotations.NotNull;
 
 public class SimplyRSCreativeTab
@@ -12,5 +14,10 @@ public class SimplyRSCreativeTab
         public ItemStack makeIcon() {
             return new ItemStack(BlockRegister.LOGICGATE_OR.get());
         }
+    };
+
+    public static final CreativeModeTab SRS_CRAFTS_TAB = new CreativeModeTab("simplyrscraftingresourcestab") {
+        @Override @NotNull
+        public ItemStack makeIcon() { return new ItemStack(ItemRegister.LOGICCORE_OR.get()); }
     };
 }
