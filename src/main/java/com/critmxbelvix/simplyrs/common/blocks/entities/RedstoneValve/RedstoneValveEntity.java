2707443f612dcca9e4f9fe0f0a6d8f1b47d0247b
Belvix <16433060+Belvix@users.noreply.github.com>
2023-01-17 19:31:05 +0530

    code cleanup. Animation changes

--- src/main/java/com/critmxbelvix/simplyrs/common/blocks/entities/RedstoneValve/RedstoneValveEntity.java
+++ src/main/java/com/critmxbelvix/simplyrs/common/blocks/entities/RedstoneValve/RedstoneValveEntity.java
@@ -28,8 +28,7 @@ public class RedstoneValveEntity extends BlockEntity implements IAnimatable {
         super(BlockEntityRegister.REDSTONE_VALVE_ENTITY.get(), pPos, pBlockState);
     }
 
-    public void use(){
-        LOGGER.info("used");
+    public void use() {
     }
 
 
@@ -42,156 +41,84 @@ public class RedstoneValveEntity extends BlockEntity implements IAnimatable {
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
         int next = this.getBlockState().getValue(RedstoneValve.OUTPUT_LEVEL);
-        if(next!=0){
+        if (next != 0) {
             controller.transitionLengthTicks = 5;
         }
-        if(next==1){
-//            if(current==0){
-//                current=next;
-//                LOGGER.info("called");
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.zero_one", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.one", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==2){
-//            if(current==1){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.one_two", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.two", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==3){
-//            if(current==2){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.two_three", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.three", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==4){
-//            if(current==3){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.three_four", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.four", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==5){
-//            if(current==4){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.four_five", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.five", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==6){
-//            if(current==5){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.five_six", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.six", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==7){
-//            if(current==6){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.six_seven", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.seven", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==8){
-//            if(current==7){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.seven_eight", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.eight", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==9){
-//            if(current==8){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.eight_nine", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.nine", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==10){
-//            if(current==9){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.nine_ten", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.ten", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==11){
-//            if(current==10){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.ten_eleven", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.eleven", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==12){
-//            if(current==11){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.eleven_twelve", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.twelve", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==13){
-//            if(current==12){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.twelve_thirteen", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.thirteen", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==14){
-//            if(current==13){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.thirteen_fourteen", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.fourteen", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==15){
-//            if(current==14){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.fourteen_fifteen", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.fifteen", ILoopType.EDefaultLoopTypes.LOOP));
-//            }
-        }
-        else if(next==0){
-//            if(current==15){
-//                current=next;
-//                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.fifteen_zero", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-//            else{
-            controller.transitionLengthTicks=0;
-                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.zero", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
-//            }
-        }
+        if (next == 1) {
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.one", ILoopType.EDefaultLoopTypes.LOOP));
+        } else if (next == 2) {
 
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.two", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 3) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.three", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 4) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.four", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 5) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.five", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 6) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.six", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 7) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.seven", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 8) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.eight", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 9) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.nine", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 10) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.ten", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 11) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.eleven", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 12) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.twelve", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 13) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.thirteen", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 14) {
+
+            controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.fourteen", ILoopType.EDefaultLoopTypes.LOOP));
+
+        } else if (next == 15) {
+            if(current==0){
+                controller.transitionLengthTicks = 0;
+                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.fifteen", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            }
+            else{
+                controller.transitionLengthTicks = 5;
+                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.fifteen", ILoopType.EDefaultLoopTypes.LOOP));
+            }
+
+        } else if (next == 0) {
+            if(current==15){
+                controller.transitionLengthTicks = 0;
+                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.zero", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            }
+            else {
+                controller.transitionLengthTicks = 5;
+                controller.setAnimation(new AnimationBuilder().addAnimation("animation.redstone_valve.zero", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            }
+        }
+        current = next;
         return PlayState.CONTINUE;
     }
 
