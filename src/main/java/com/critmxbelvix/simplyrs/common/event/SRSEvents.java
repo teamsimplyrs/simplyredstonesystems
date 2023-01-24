ec716e4bbb9bac23dbf6d3efac3876c96f2dc6b3
CriTMx <savefailedmc@gmail.com>
2023-01-24 13:01:36 +0530

    changed events class name

--- src/main/java/com/critmxbelvix/simplyrs/common/event/SRSEvents.java
+++ src/main/java/com/critmxbelvix/simplyrs/common/event/SRSEvents.java
@@ -10,7 +10,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = SimplyRedstoneSystems.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class ModEventClientBusEvents {
+public class SRSEvents {
     @SubscribeEvent
     public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(BlockEntityRegister.REDSTONE_CLOCK_ENTITY.get(), RedstoneClockRenderer::new);
