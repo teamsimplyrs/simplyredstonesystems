3b9b9096d5df7575f10bbc0d575eef18b88c82e9
Belvix <16433060+Belvix@users.noreply.github.com>
2023-02-04 23:58:10 +0530

    Fixed the most annoying bug with sprites not spawning

--- src/main/java/com/critmxbelvix/simplyrs/common/event/SRSEvents.java
+++ src/main/java/com/critmxbelvix/simplyrs/common/event/SRSEvents.java
@@ -12,11 +12,9 @@ import com.critmxbelvix.simplyrs.common.registers.BlockRegister;
 import com.critmxbelvix.simplyrs.common.registers.ItemRegister;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -26,10 +24,8 @@ import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.IModBusEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import software.bernie.shadowed.eliotlash.mclib.math.functions.limit.Min;
 
 @Mod.EventBusSubscriber(modid = SimplyRedstoneSystems.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class SRSEvents {
@@ -44,16 +40,34 @@ public class SRSEvents {
     public static void activatorMineSummonSpriteEvent(BlockEvent.BreakEvent breakEvent)
     {
         Item tool = breakEvent.getPlayer().getMainHandItem().getItem();
-        boolean toolIsValid = tool instanceof ActivatorAxe || tool instanceof ActivatorPickaxe || tool instanceof ActivatorHoe || tool instanceof ActivatorShovel;
         Level level = breakEvent.getPlayer().getLevel();
-        BlockPos breakPos = breakEvent.getPos();
-        LOGGER.info("Tool: "+tool+"\nIs tool valid?"+toolIsValid);
-        if (toolIsValid)
-        {
-            level.setBlock(breakPos, BlockRegister.REDSTONE_SPRITE.get().defaultBlockState(),2);
-            LOGGER.info("New block in place of broken block: "+level.getBlockState(breakPos));
+        if(!level.isClientSide()){
+            boolean toolIsValid = tool instanceof ActivatorAxe || tool instanceof ActivatorPickaxe || tool instanceof ActivatorHoe || tool instanceof ActivatorShovel;
+            BlockPos breakPos = breakEvent.getPos();
+            LOGGER.info("Tool: "+tool+"\nIs tool valid?"+toolIsValid);
+            if (toolIsValid)
+            {
+                breakEvent.setCanceled(true);
+                level.destroyBlock(breakPos,true);
+                level.setBlockAndUpdate(breakPos, BlockRegister.REDSTONE_SPRITE.get().defaultBlockState());
+                LOGGER.info("New block in place of broken block: "+level.getBlockState(breakPos));
+            }
         }
     }
+
+//    public static void leftClickEvent(BlockEvent. )
+//    {
+//        Item tool = breakEvent.getPlayer().getMainHandItem().getItem();
+//        boolean toolIsValid = tool instanceof ActivatorAxe || tool instanceof ActivatorPickaxe || tool instanceof ActivatorHoe || tool instanceof ActivatorShovel;
+//        Level level = breakEvent.getPlayer().getLevel();
+//        BlockPos breakPos = breakEvent.getPos();
+//        LOGGER.info("Tool: "+tool+"\nIs tool valid?"+toolIsValid);
+//        if (toolIsValid)
+//        {
+//            level.setBlock(breakPos, BlockRegister.REDSTONE_SPRITE.get().defaultBlockState(),2);
+//            LOGGER.info("New block in place of broken block: "+level.getBlockState(breakPos));
+//        }
+//    }
 
     public static void activatorBootsFallDamageEvent(LivingFallEvent fallEvent)
     {
